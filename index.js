1ba12c1e6c37878eb9bcb09311c0258247b24c61
manhdino <manhnguyen1238@gmail.com>
2024-07-23 14:33:29 +0700

    services clusterIP Kubernetes

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer((req, res) => {
-    res.end("Hello kube\n")
-});
+// const server = http.createServer((req, res) => {
+//     res.end("Hello kube\n")
+// });
 
-server.listen(3000, () => {
-    console.log("Server listen on port 3000")
+// server.listen(3000, () => {
+//     console.log("Server listen on port 3000")
+// })
+
+
+const redis = require("redis");
+
+const client = redis.createClient("redis://redis:6379")
+
+client.on("connect", () => {
+    console.log("Connect redis success");
 })
+
