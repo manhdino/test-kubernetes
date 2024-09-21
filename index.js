b08fa134741cf706318a86ddbcb41f3998f64fbb
manh dino <manhnguyen1238@gmail.com>
2024-09-21 13:04:04 +0700

    add backup data

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer((req, res) => {
-    res.end("Hello kube\n")
-});
+// const server = http.createServer((req, res) => {
+//     res.end("Hello kube\n")
+// });
 
-server.listen(3000, () => {
-    console.log("Server listen on port 3000")
-})
+// server.listen(3000, () => {
+//     console.log("Server listen on port 3000")
+// })
 
 
 // const redis = require("redis");
@@ -17,3 +17,25 @@ server.listen(3000, () => {
 //     console.log("Connect redis success");
 // })
 
+
+
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//     res.end("Hello application v1\n")
+// });
+
+// server.listen(3000, () => {
+//     console.log("Server listen on port 3000")
+// })
+
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.end("Hello application v2\n") // change v1 to v2
+});
+
+server.listen(3000, () => {
+    console.log("Server listen on port 3000")
+})
